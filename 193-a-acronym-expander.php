b6a85a7c9d4d668c50c263bd3b8d199697402722
gma992 <gabrielmaldonado.art@gmail.com>
2014-12-23 10:53:13 +0100

    Update 193-a-acronym-expander.php

--- 193-a-acronym-expander.php
+++ 193-a-acronym-expander.php
@@ -1,5 +1,32 @@
 <?php
-
 //[2014-12-19] Challenge #193 [Easy] Acronym Expander
+
+/*
+Description
+
+During online gaming (or any video game that requires teamwork) , there is often times that you need to speak to your teammates. Given the nature of the game, it may be inconvenient to say full sentences and it's for this reason that a lot of games have acronyms in place of sentences that are regularly said.
+Example
+gg : expands to 'Good Game'
+brb : expands to 'be right back'
+and so on...
+This is even evident on IRC's and other chat systems. However, all this abbreviated text can be confusing and intimidating for someone new to a game. They're not going to instantly know what 'gl hf all'(good luck have fun all) means. It is with this problem that you come in.
+You are tasked with converting an abbreviated sentence into its full version.
+
+Input: On console input you will be given a string that represents the abbreviated chat message.
+Output: should consist of the expanded sentence
+
+Wordlist
+
+lol - laugh out loud
+dw - don't worry
+hf - have fun
+gg - good game
+brb - be right back
+g2g - got to go
+wtf - what the fuck
+wp - well played
+gl - good luck
+imo - in my opinion
+*/
 
 ?>
